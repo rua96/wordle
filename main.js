a35acd50ad8798e87b98640cca52739b2b02a947
rua96 <rua96@hotmail.it>
2024-06-03 12:58:43 +0200

    Nuova parola

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-function onClick(){
-let parola="apple";
-let input = document.getElementById("input").value;
-if (parola===input){
-    myColor="green";}
-else{
-    myColor="red";
+let parola = "cream"
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2");
+let input3 = document.getElementById("input3");
+let input4 = document.getElementById("input4");
+let input5 = document.getElementById("input5");
+input1.addEventListener("input", (e) => updateValue(e,input1));
+input2.addEventListener("input", (e) => updateValue(e,input2));
+input3.addEventListener("input", (e) => updateValue(e,input3));
+input4.addEventListener("input", (e) => updateValue(e,input4));
+input5.addEventListener("input", (e) => updateValue(e,input5));
+
+function updateValue(event,input) {
+    input.value = event.target.value.slice(-1);
 }
-document.body.style.backgroundColor=myColor;
-console.log("Input Value", input);
-};
+function onClick() {
+        let word = input1.value + input2.value + input3.value + input5.value + input5.value;
+        console.log("word",word)
+}
