b6b401faaa5cdbe9008ea290e2c3e39e3a419c7f
rua96 <rua96@hotmail.it>
2024-06-03 10:05:44 +0200

    messaggio

---
+++ main.js
@@ -0,0 +1,12 @@
+let myColor="red"
+function onClick(){
+if(myColor==="red")
+    {
+        myColor="blue"
+    }
+    else{
+        myColor="red"
+    }
+
+document.body.style.backgroundColor=myColor;
+}
