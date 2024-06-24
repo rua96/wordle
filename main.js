55d59d7421ec151ad259edc99ca970715f308623
rua96 <rua96@hotmail.it>
2024-06-24 12:54:21 +0200

    24giugno

--- main.js
+++ main.js
@@ -6,13 +6,33 @@ let wordInputArray = [
     [input16,input17,input18,input19,input20],
     [input21,input22,input23,input24,input25]
 ]
+
+document.addEventListener('keydown',
+function(event){ 
+if(event.key==='Backspace') {
+   if(event.target.previousElementSibling && !event.target.value)
+       event.target.previousElementSibling.focus();
+    }
+});
+
+
 function onInput(e) {
-    e.target.value = e.target.value.slice(-1);
+     if(e.inputType === 'deleteContentBackward') {
+        return;
+     } else {
+        if(e.target.nextElementSibling){
+            e.target.nextElementSibling.focus();
+        }
+        e.target.value = e.target.value.slice(-1);
+     }    
 }
+
 function onSubmit(e,wordIndex) {
     e.preventDefault();
     console.log("onSìsubmit")
         let word = wordInputArray[wordIndex];
+        let correct = 0;
+        if(word.length<5){ word[i].disabled = false;}
         for(let i=0; i< word.length; i++) {
             if(word[i].value==parola[i])
             {
@@ -24,6 +44,17 @@ function onSubmit(e,wordIndex) {
             }
             else
             {
-                 word[i].style.backgroundColor = "grey"}
+                 word[i].style.backgroundColor = "grey"
+            } word[i].disabled = true;
             }
+            let nextLine = wordInputArray[wordIndex+1];
+            for(i=0; i<nextLine.length; i++) {
+               nextLine[i].disabled = false; 
+            }
+        nextLine[0].focus()
 }
+
+window.onload=()=> {
+   wordInputArray[0][0].focus()
+    };
+
